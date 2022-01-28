4a53a07d685cbe99301813ebd25ed80c2e5f1539
Adrish Dey <adrishd.cse2017@nsec.ac.in>
2022-01-28 20:37:13 +0530

    fixing issue with channels

--- model.py
+++ model.py
@@ -5,11 +5,16 @@ from torch import nn
 class ConvNet(nn.Module):
     def __init__(self, image_dim, num_classes, norm_type, use_norm=False, **kwargs):
         super().__init__()
+        channels = image_dim.pop()
         hook = normalizations.NORMALIZATIONS[norm_type](image_dim, **kwargs)
         self.layer1 = nn.Sequential(
             *hook.conv_hook(
                 nn.Conv2d(
-                    1, kwargs["l1_size"], kwargs["kernel_size"], stride=1, padding=2
+                    channels,
+                    kwargs["l1_size"],
+                    kwargs["kernel_size"],
+                    stride=1,
+                    padding=2,
                 ),
                 norm=use_norm,
             ),
